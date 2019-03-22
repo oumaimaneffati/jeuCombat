c6c92d9c719233e5d2f8810a6013316427ebfadb
Unknown <semgjj@yahoo.fr>
2019-03-22 05:11:31 +0100

    testing

--- tests/UserBundle/Controller/DefaultControllerTest.php
+++ tests/UserBundle/Controller/DefaultControllerTest.php
@@ -12,6 +12,17 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Welcome to the world\'s fiercest coliseum', $crawler->filter('h2')->text());
+    }
+
+    public function testLoginForm()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+        var_dump($crawler);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
